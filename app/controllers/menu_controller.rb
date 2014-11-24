2c12c1ed4395afa84ae2d66d40d775a491e16279
Yao Ming <yao@localhost.localdomain>
2014-11-23 23:02:12 -0300

    Haciendo relaciones entre tablas User, Tropa y Recurso

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -4,6 +4,10 @@ class MenuController < ApplicationController
 
   def menu_user
   	@user= User.where(:email => current_user.email)
+  	@recursos = User.find(current_user.id).recursos
+  	@tropas = User.find(current_user.id).tropas
+  	#@prueba = Recurso.find(1)
+  	#@resultado = RecursoUser.where(:recursos_id => @prueba.id, :user.id => @user.id)
 
   end
 
